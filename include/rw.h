1b84bb2e55149ce1b84ef5c6f453afe9cb507f1e
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-25 16:17:25 +0300

    Update rw.h

--- include/rw.h
+++ include/rw.h
@@ -1,7 +1,7 @@
 #ifndef RW_H
 #define RW_H
 void write(const char* data);
-void init(void);
+void init(char* fg, char* fg);
 void printart();
 void clsdrv();
 void terminal_setcolor(int colorbg, int colorfg);
