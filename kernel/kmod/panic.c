6fcac7646f0ff49147274f70ecd0e64931313cab
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-25 15:01:03 +0300

    Update panic.c

--- kernel/kmod/panic.c
+++ kernel/kmod/panic.c
@@ -1,6 +1,6 @@
 #include <stdint.h>
 #include "rw.h"
 void oops(char* errmsg){
-    write('An error occured!\nThe error code is');
+    write("An error occured!\nThe error code is");
     write(errmsg);
 }
