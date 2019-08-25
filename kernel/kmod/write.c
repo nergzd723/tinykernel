f09b32bd74249d0a979a990bf09a8a337ce695f3
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-25 14:59:30 +0300

    Update write.c

--- kernel/kmod/write.c
+++ kernel/kmod/write.c
@@ -77,9 +77,11 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)             
 void terminal_putchar(char c) 
 {
 	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-    if (c == '/n'):
-        terminal_row++;
-        terminal_column = 0;                                                                              //print char to screen 
+    	if (c == '/n'){
+        	terminal_row++;
+		terminal_column = 0; 
+	}
+	//print char to screen 
 	if (++terminal_column == VGA_WIDTH) {
 		terminal_column = 0;
 		if (++terminal_row == VGA_HEIGHT)
