6c8e0ee377026d3a3cdce8c5aa5f774b0710a50c
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-25 18:27:35 +0300

    Update panic.c

--- kernel/kmod/panic.c
+++ kernel/kmod/panic.c
@@ -2,9 +2,9 @@
 #include "rw.h"
 void oops(char* errmsg){
     clsdrv();
-    write("\n\n\n\n\n\n\n\n\n\n\n\n");
+    write("\n\n\n\n\n\n\n\n\n\n");
     write("An error occured!\nThe error code is");
     write("\n\n");
     write(errmsg);
-    while(1) {}
+    while(1) { }
 }
