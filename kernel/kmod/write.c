391e181bb0b7909feb1e9a18bed0a4650ecaa72f
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-25 16:17:00 +0300

    Update write.c

--- kernel/kmod/write.c
+++ kernel/kmod/write.c
@@ -49,11 +49,11 @@ size_t terminal_column;
 uint8_t terminal_color;
 uint16_t* terminal_buffer;
 
-void init(void) 
+void init(char* bg, char* fg) 
 {
 	terminal_row = 0;
 	terminal_column = 0;
-	terminal_color = vga_entry_color(VGA_COLOR_CYAN, VGA_COLOR_WHITE);            //init a terminal
+	terminal_color = vga_entry_color(bg, fg);            //init a terminal
 	terminal_buffer = (uint16_t*) 0xB8000;
 	for (size_t y = 0; y < VGA_HEIGHT; y++) {
 		for (size_t x = 0; x < VGA_WIDTH; x++) {
