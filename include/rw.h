211846ab7e9c4c70aebdfaed860b225783f00c23
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-25 16:24:19 +0300

    Update rw.h

--- include/rw.h
+++ include/rw.h
@@ -1,7 +1,7 @@
 #ifndef RW_H
 #define RW_H
 void write(const char* data);
-void init(char* cfg, char* cbg);
+void init(enum vga_color cfg, enum vga_color cbg);
 void printart();
 void clsdrv();
 void terminal_setcolor(int colorbg, int colorfg);
