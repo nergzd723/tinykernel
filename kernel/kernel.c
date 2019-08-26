5246f8370415f877e2e26202f8b62f57412bddd1
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-26 18:14:34 +0300

    Update kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -4,22 +4,22 @@
 #include "kernel.h"
 #include "rw.h"
 #include "video.h"
+
 int divide(int a, int b)
 {
 	int c;
 	c = a/b;
 	return c;
 }
+
 void kernel_main(void) 
 {
 	init();
-	long b = 0;
-	int a = 1/0;
 	for (long i = 0; i<1000000000; i++)
 	{
 		b++;
 		writed(b);
-		write('\n');
+		write("\n");
 	}
 }
  
