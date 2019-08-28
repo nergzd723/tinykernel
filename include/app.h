df9a68cb4284d4e42226290deb8dade8fa671110
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-28 17:48:41 +0300

    Update and rename call_app.h to app.h

--- include/app.h
+++ include/app.h
@@ -4,11 +4,20 @@
 #include "kernel.h"
 #include "rw.h"
 #include "serial.h"
-void call_app(bool m)
+#include "video.h"
+void call_app(int m)
 {
-  if(m)
+  if(m == 1)
   {
     printart();
+  }
+  else if (m == 2)
+  {
+    clsdrv();
+  }
+  else if (m == 3)
+  {
+    matrix();
   }
   else
   {
