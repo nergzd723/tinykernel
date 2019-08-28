f5db106cfdd67bb80ce3fb52cade49c135a57a0f
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-28 17:50:44 +0300

    Update video.h

--- include/video.h
+++ include/video.h
@@ -1,31 +1,10 @@
-enum vga_color {                    //some VGA color enum
-	VGA_COLOR_BLACK = 0,
-	VGA_COLOR_BLUE = 1,
-	VGA_COLOR_GREEN = 2,
-	VGA_COLOR_CYAN = 3,
-
-	VGA_COLOR_RED = 4,
-
-	VGA_COLOR_MAGENTA = 5,
-
-	VGA_COLOR_BROWN = 6,
-
-	VGA_COLOR_LIGHT_GREY = 7,
-
-	VGA_COLOR_DARK_GREY = 8,
-
-	VGA_COLOR_LIGHT_BLUE = 9,
-
-	VGA_COLOR_LIGHT_GREEN = 10,
-
-	VGA_COLOR_LIGHT_CYAN = 11,
-
-	VGA_COLOR_LIGHT_RED = 12,
-
-	VGA_COLOR_LIGHT_MAGENTA = 13,
-
-	VGA_COLOR_LIGHT_BROWN = 14,
-
-	VGA_COLOR_WHITE = 15,
-
-};
+#include "rw.h"
+void matrix()
+{
+	long b = 0;
+	for (long i = 0; i < 1000000000; i++)
+	{
+		writed(b);
+		b++;
+	}
+}
