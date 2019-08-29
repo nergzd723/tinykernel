53fc438f08a6b7210b722a032f91e9c3aa7034b5
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-29 21:51:08 +0300

    Update irq.c

--- kernel/kmod/irq.c
+++ kernel/kmod/irq.c
@@ -1,6 +1,8 @@
 #include "dsp.h"
 #include "rw.h"
 #include "types.h"
+#include "irq.h"
+
 void interrupt_handler(uint32_t interrupt_number, uint32_t error_code)
 {
   warn("Got IRQ, DINFO in serial");
@@ -10,5 +12,8 @@ void interrupt_handler(uint32_t interrupt_number, uint32_t error_code)
   log(interrupt_number);
   return;
 }
-void load_idt(void * idt);
-void interrupt(uint32_t interrupt_number);
+void enable_keyboard_interrupts() {
+  outb(0x21,0xfd);
+  outb(0xa1,0xff);
+  enable_hardware_interrupts();
+}
