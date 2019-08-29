9e5d9d91a8566cfa2aec9330cc292700e52efb6c
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-29 16:22:53 +0300

    Update serial.h

--- include/serial.h
+++ include/serial.h
@@ -1,7 +1,12 @@
 #ifndef SERIAL_H
 #define SERIAL_H
-#include "rw.h"
-#include "serialvar.h"
+#define SERIAL_COM1_BASE                0x3F8
+#define SERIAL_DATA_PORT(base)          (base)
+#define SERIAL_FIFO_COMMAND_PORT(base)  (base + 2)
+#define SERIAL_LINE_COMMAND_PORT(base)  (base + 3)
+#define SERIAL_MODEM_COMMAND_PORT(base) (base + 4)
+#define SERIAL_LINE_STATUS_PORT(base)   (base + 5)
+#define SERIAL_LINE_ENABLE_DLAB         0x80
 void serial_configure_baud_rate(unsigned short com, unsigned short divisor)
 {
     outb(SERIAL_LINE_COMMAND_PORT(com),
