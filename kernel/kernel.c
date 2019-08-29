70e34009c0baea3116a9cbda3e6472ba9a6ce38b
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-29 19:12:35 +0300

    Update kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -2,47 +2,13 @@
 #include <stddef.h>
 #include <stdint.h>
 #include "kernel.h"
-#include "rw.h"
-#include "serialif.h"
 #include "app.h"
-struct gdt_t {
-  unsigned int address;
-  unsigned short size;
-} __attribute__((packed)) gdt;
 
-struct segment_selector {
-  unsigned short address;
-  unsigned short size;
-} __attribute__((packed));
-
-unsigned short segment_selectors[] = {
-  0x0, // null descriptor
-  (0x08 << 4), // kernel code segment
-  (0x10 << 4), // kernel data segment
-};
-
-void initialize_gdt() {
-  gdt.address = (unsigned short) segment_selectors;
-  gdt.size = sizeof(segment_selectors);
-  lgdt(&gdt);
-}
 void kernel_main(void) 
 {
-	serial_init(SERIAL_COM1_BASE);
-	log(SERIAL_COM1_BASE, "Hello World!\n");
-	_Bool ok = init();
-	initialize_gdt();
-	log(SERIAL_COM1_BASE, "Loaded global descriptor table.\n");
-	
-	if(ok)
-	{
-		log(SERIAL_COM1_BASE, "Loading app\n");
-		write("Kernel module OK. Loading app...");
-		call_app(4);
-	}
-	else
-	{
-		oops("TERMINAL_ERR");
-	}
+	init();
+	log("Loading app\n");
+	printf("Kernel module OK. Loading app...");
+	call_app(4);
 }
  
