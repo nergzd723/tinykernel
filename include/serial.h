5a30176d643c6b2ad7de2fdab9adf5036b82ccd9
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-29 17:43:55 +0300

    Update serial.h

--- include/serial.h
+++ include/serial.h
@@ -46,7 +46,7 @@ int serial_is_transmit_fifo_empty(unsigned int com)
 
 }
 
-void serial_print(unsigned short com, char * s) {
+void log(unsigned short com, char * s) {
 
     int i = 0;
 
@@ -56,7 +56,7 @@ void serial_print(unsigned short com, char * s) {
         i++;
     }
 }
-void serial_write_char(unsigned short com, char c) {
+void logc(unsigned short com, char c) {
     while (!serial_is_transmit_fifo_empty(com)) {}
     outb(SERIAL_DATA_PORT(com), c);
 }
