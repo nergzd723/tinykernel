d5784f1672108738d9746aad378146998a04b261
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-29 17:41:50 +0300

    Update serialif.h

--- include/serialif.h
+++ include/serialif.h
@@ -3,5 +3,5 @@
 #define SERIAL_COM1_BASE                0x3F8 
 void serial_init(unsigned short com);
 void serial_print(unsigned short com, char * s);
-
+void serial_write_char(unsigned short com, char c);
 #endif
