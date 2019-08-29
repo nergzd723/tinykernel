1b2351a0a9e0efff44e3c462b87f0b7c71f352c3
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-29 16:10:43 +0300

    Update kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -10,12 +10,10 @@ void kernel_main(void)
 	serial_init(SERIAL_COM1_BASE);
 	serial_print(SERIAL_COM1_BASE, "Hello World!\n");
 	_Bool ok = init();
-	int pr = 3;
 	if(ok)
 	{
-		serial_print(SERIAL_COM1_BASE, "Loading app");
 		write("Kernel module OK. Loading app...");
-		call_app(pr);
+		call_app(3);
 	}
 	else
 	{
