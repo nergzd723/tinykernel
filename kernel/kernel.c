7b8b8f7de0340931b8a5ed1ed02443f080f6d6b7
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-29 19:43:43 +0300

    Update kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -4,25 +4,47 @@
 #include "oopsh.h"
 #include "app.h"
 #include "dsph.h"
+
 struct gdt_t {
   unsigned int address;
   unsigned short size;
 } __attribute__((packed)) gdt;
 
-struct segment_selector {
-  unsigned short address;
-  unsigned short size;
+
+struct segment_descriptor_t {
+  int16 base_0_15; // bits 0-15 of base
+  int16 limit_0_15;
+  int8 base_24_31;
+  int8 flags_and_limit_16_19;
+  int8 access_byte;
+  int8 base_16_23;
 } __attribute__((packed));
 
-unsigned short segment_selectors[] = {
-  0x0, // null descriptor
-  (0x08 << 4), // kernel code segment
-  (0x10 << 4), // kernel data segment
+enum segment_selector_t {
+  NULL_DESCRIPTOR, // Not but has to be here
+  KERNAL_CODE_SEGMENT, // Offset 0x8
+  KERNAL_DATA_SEGMENT // Offset 0x10
 };
 
+struct segment_descriptor_t segment_descriptors[3];
+
 void initialize_gdt() {
-  gdt.address = (unsigned short) segment_selectors;
-  gdt.size = sizeof(segment_selectors);
+
+  gdt.address = (int16) segment_descriptors;
+  gdt.size = sizeof(segment_descriptors);
+  segment_descriptors[KERNAL_CODE_SEGMENT].base_0_15 = 0x0;
+  segment_descriptors[KERNAL_CODE_SEGMENT].base_24_31 = 0x0;
+  segment_descriptors[KERNAL_CODE_SEGMENT].limit_0_15 = 0xFFFF;
+  segment_descriptors[KERNAL_CODE_SEGMENT].flags_and_limit_16_19 = 0b1000 << 4;
+  segment_descriptors[KERNAL_CODE_SEGMENT].flags_and_limit_16_19 &= 0xF;
+  segment_descriptors[KERNAL_CODE_SEGMENT].access_byte &= 0b10010110;
+
+  segment_descriptors[KERNAL_DATA_SEGMENT].base_0_15 = 0x0;
+  segment_descriptors[KERNAL_DATA_SEGMENT].base_24_31 = 0x0;
+  segment_descriptors[KERNAL_DATA_SEGMENT].limit_0_15 = 0xFFFF;
+  segment_descriptors[KERNAL_DATA_SEGMENT].flags_and_limit_16_19 = 0b1000 << 4;
+  segment_descriptors[KERNAL_DATA_SEGMENT].flags_and_limit_16_19 &= 0xF;
+  segment_descriptors[KERNAL_DATA_SEGMENT].access_byte &= 0b10010010;
   lgdt(&gdt);
 }
 
