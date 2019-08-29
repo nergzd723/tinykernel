a1492131495c8fb2a65f5e2ced6d7bbfe02ee64e
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-29 16:15:19 +0300

    Update panic.c

--- kernel/kmod/panic.c
+++ kernel/kmod/panic.c
@@ -3,8 +3,6 @@
 #include "kernel.h"
 #include "serial.h"
 void oops(char* errmsg){
-    serial_print(SERIAL_COM1_BASE, "Got exception");
-    serial_print(SERIAL_COM1_BASE, errmsg);
     clsdrv();
     write("\n\n\n\n\n\n\n\n\n\n");
     write("An error occured!\nThe error code is");
