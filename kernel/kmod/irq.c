f2af77e7b879e15978a542716a5950e540758126
Mark <clashclanacc2602@gmail.com>
2019-08-29 21:31:53 +0300

    fix

---
+++ kernel/kmod/irq.c
@@ -0,0 +1,13 @@
+#include "dsp.h"
+#include "rw.h"
+void interrupt_handler(uint32_t interrupt_number, uint32_t error_code)
+{
+  warn("Got IRQ, DINFO in serial");
+  log("error_code: ");
+  log(error_code);
+  log("interrupt_number: ");
+  log(interrupt_number);
+  return;
+}
+void load_idt(void * idt);
+void interrupt(uint32_t interrupt_number);
