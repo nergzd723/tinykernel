d268215bb2a42ce06f7ac50a870fcec7c6f6a1be
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-29 21:49:21 +0300

    Update irq.h

--- include/irq.h
+++ include/irq.h
@@ -2,3 +2,7 @@
 void interrupt_handler(uint32_t interrupt_number, uint32_t error_code);
 void load_idt(void * idt);
 void interrupt(uint32_t interrupt_number);
+void enable_hardware_interrupts();
+
+
+
