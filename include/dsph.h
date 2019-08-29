98138c5b44aff77d4f5f0d9c0070276b1df0bdfd
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-29 19:09:57 +0300

    Create dsph.h

---
+++ include/dsph.h
@@ -0,0 +1,26 @@
+#ifndef LOGH_H
+#define LOGH_H
+#include "rw.h"
+#include "serialif.h"
+#include "oopsh.h"
+void init()
+{
+  t_init();
+  serial_init(SERIAL_COM1_BASE);
+}
+void log(char* data)
+{
+  serial_write(SERIAL_COM1_BASE, data);
+}
+void printf(char* data)
+{
+  write(data);
+  log("Wrote to screen")
+}
+void warn(char* data)
+{
+  log(data);
+  write("\n\n\nWarning!");
+  write(data);
+}
+#endif
