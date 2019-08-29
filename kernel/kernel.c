adb2e70fa26b5db0480d960893c2d75452ce9af4
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-29 19:20:34 +0300

    Update kernel.c

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -4,10 +4,32 @@
 #include "oopsh.h"
 #include "app.h"
 #include "dsph.h"
+struct gdt_t {
+  unsigned int address;
+  unsigned short size;
+} __attribute__((packed)) gdt;
+
+struct segment_selector {
+  unsigned short address;
+  unsigned short size;
+} __attribute__((packed));
+
+unsigned short segment_selectors[] = {
+  0x0, // null descriptor
+  (0x08 << 4), // kernel code segment
+  (0x10 << 4), // kernel data segment
+};
+
+void initialize_gdt() {
+  gdt.address = (unsigned short) segment_selectors;
+  gdt.size = sizeof(segment_selectors);
+  lgdt(&gdt);
+}
 
 void kernel_main(void) 
 {
 	init();
+	initialize_gdt();
 	log("Loading app\n");
 	printf("Kernel module OK. Loading app...");
 	call_app(4);
