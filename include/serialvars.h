59b605a82753dba8e6ab1f131a88f365ddf75a99
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-29 16:19:00 +0300

    Create serialvars.h

---
+++ include/serialvars.h
@@ -0,0 +1,16 @@
+#ifndef SERIALVARS_H
+#define SERIALVARS_H
+#define SERIAL_COM1_BASE                0x3F8
+
+#define SERIAL_DATA_PORT(base)          (base)
+
+#define SERIAL_FIFO_COMMAND_PORT(base)  (base + 2)
+
+#define SERIAL_LINE_COMMAND_PORT(base)  (base + 3)
+
+#define SERIAL_MODEM_COMMAND_PORT(base) (base + 4)
+
+#define SERIAL_LINE_STATUS_PORT(base)   (base + 5)
+
+#define SERIAL_LINE_ENABLE_DLAB         0x80
+#endif
