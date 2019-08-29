dd904be83aa4184d203086c6043ab9ccd1655209
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-29 21:16:23 +0300

    Update irq.h

--- include/irq.h
+++ include/irq.h
@@ -1,7 +1,13 @@
 #include "dsp.h"
-void interrupt_handler()
+#include "rw.h"
+void interrupt_handler(struct cpu_state cpu, uint32_t interrupt_number, uint32_t error_code)
 {
-  oops("Got IRQ");
+  warn("Got IRQ, DINFO in serial");
+  log("error_code: ");
+  log(error_code);
+  log("interrupt_number: ");
+  log(interrupt_number);
+  return;
 }
 void load_idt(void * idt);
 void interrupt(uint32_t interrupt_number);
