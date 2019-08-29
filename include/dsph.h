f9eeaa78dded39f8b8b06ec6ad24d8124294adb3
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-29 19:19:34 +0300

    Update dsph.h

--- include/dsph.h
+++ include/dsph.h
@@ -3,33 +3,11 @@
 #include "rw.h"
 #include "serialif.h"
 #include "oopsh.h"
-struct gdt_t {
-  unsigned int address;
-  unsigned short size;
-} __attribute__((packed)) gdt;
-
-struct segment_selector {
-  unsigned short address;
-  unsigned short size;
-} __attribute__((packed));
-
-unsigned short segment_selectors[] = {
-  0x0, // null descriptor
-  (0x08 << 4), // kernel code segment
-  (0x10 << 4), // kernel data segment
-};
-
-void initialize_gdt() {
-  gdt.address = (unsigned short) segment_selectors;
-  gdt.size = sizeof(segment_selectors);
-  lgdt(&gdt);
-}
 
 void init()
 {
   t_init();
   serial_init(SERIAL_COM1_BASE);
-  initialize_gdt();
   log("GDT, terminal and serial init!");
 }
 void log(char* data)
