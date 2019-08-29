b213e9fedbe462ba25b3f0d296d5d6ae0bfbf5c3
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-29 16:29:19 +0300

    Update panic.c

--- kernel/kmod/panic.c
+++ kernel/kmod/panic.c
@@ -1,11 +1,15 @@
 #include <stdint.h>
 #include "rw.h"
 #include "kernel.h"
+#include "serialif.h"
 void oops(char* errmsg){
+    serial_print(SERIAL_COM1_BASE, "Got exception, \n Panicked successfully!");
+    serial_print(SERIAL_COM1_BASE, errmsg);
     clsdrv();
     write("\n\n\n\n\n\n\n\n\n\n");
     write("An error occured!\nThe error code is");
     write("\n\n");
     write(errmsg);
+    serial_print(SERIAL_COM1_BASE, "Halting the CPU...");
     halt();
 }
