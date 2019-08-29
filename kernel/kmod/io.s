f140aafc79bd7b260d218e99427aee04995a0970
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-29 21:04:40 +0300

    Update io.s

--- kernel/kmod/io.s
+++ kernel/kmod/io.s
@@ -24,3 +24,34 @@ global interrupt
 interrupt:
   mov eax, [esp+4]
   int 49
+extern interrupt_handler
+global interrupt_handler_49
+interrupt_handler_49:
+  push    dword 0
+  push    dword 49
+  jmp     common_interrupt_handler
+
+common_interrupt_handler:
+  push    eax
+  push    ebx
+  push    ecx
+  push    edx
+  push    esi
+  push    edi
+  push    esp
+  push    ebp
+  
+  call    interrupt_handler
+  
+  pop    ebp
+  pop    esp
+  pop    edi
+  pop    esi
+  pop    edx
+  pop    ecx
+  pop    ebx
+  pop    eax
+  
+  add     esp, 8
+  
+  iret
