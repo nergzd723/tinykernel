98f75aad33e844236e02716bf4feae4568278bbf
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-29 21:37:47 +0300

    Update types.h

--- include/types.h
+++ include/types.h
@@ -14,4 +14,8 @@ typedef unsigned short int16;
 typedef int16 byte2;
 typedef unsigned long int32;
 typedef int16 byte4;
+
+typedef unsigned char uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned long uint32_t;
 #endif
