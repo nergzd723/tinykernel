11809a2019bb543105bd8eb43dff933682087d98
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-30 19:05:33 +0300

    Update irq_n.h

--- include/irq_n.h
+++ include/irq_n.h
@@ -1,5 +1,3 @@
-#ifndef INCLUDE_INTERRUPTS_H
-#define INCLUDE_INTERRUPTS_H
 #include "types.h"
 struct stack_state {
   uint32_t error_code;
@@ -8,4 +6,3 @@ struct stack_state {
   uint32_t eflags;
 } __attribute__((packed));
 void interrupt_handler(uint32_t interrupt_number, uint32_t error_code);
-#endif /* INCLUDE_INTERRUPTS_H */
