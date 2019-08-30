1c90327141b1b5ffd5c059df0e9710bbdbdc1ac0
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-30 19:00:29 +0300

    Update kbd.h

--- include/kbd.h
+++ include/kbd.h
@@ -1,6 +1,6 @@
 
-#ifndef INCLUDE_KEYBOARD_H
-#define INCLUDE_KEYBOARD_H
+#ifndef INCLUDE_KBD_H
+#define INCLUDE_KBD_H
 #include "types.h"
 uint8_t consume_scan_code();
 #endif /* INCLUDE_KEYBOARD_H */ 
