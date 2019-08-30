4125ab156326bd5d8bb1337017eae60bf4a8a26c
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-30 19:12:51 +0300

    Update rw.h

--- include/rw.h
+++ include/rw.h
@@ -5,8 +5,6 @@ void t_init(void);
 void printart();
 void clsdrv();
 void lgdt(void * gdt);
-void outb(unsigned short port, unsigned char data);
-unsigned char inb(unsigned short port);
 void serial_print(unsigned short com, char * s);
 void writed(const int data);
 void terminal_setcolor(int colorbg, int colorfg);
