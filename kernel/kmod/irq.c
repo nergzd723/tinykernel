3899426e34e2619f5492a64456c62e22ad69df76
Mark <53225591+nergzd723@users.noreply.github.com>
2019-08-31 17:14:09 +0300

    Update irq.c

--- kernel/kmod/irq.c
+++ kernel/kmod/irq.c
@@ -16,7 +16,8 @@ void interrupt_handler(uint32_t interrupt_number, uint32_t error_code)
       log("returned from pic_acknowledge()");
       break;
     default:
-      pic_acknowledge();
+      //pic_acknowledge();
+      logd(interrupt_number);
       break;
   }
   return;
