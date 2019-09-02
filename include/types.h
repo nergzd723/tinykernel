d30623fca28962efc1e22b68e958d50744a90743
Mark <53225591+nergzd723@users.noreply.github.com>
2019-09-02 15:40:18 +0300

    Update types.h

--- include/types.h
+++ include/types.h
@@ -3,17 +3,13 @@
 #define TYPES_H
 
 typedef unsigned int   u32;
-
 typedef          int   s32;
-
 typedef unsigned short u16;
-
 typedef          short s16;
-
 typedef unsigned char  u8;
-
 typedef          char  s8;
-
+typedef unsigned int int16;
+typedef unsigned int int8;
 #define low_16(address) (u16)((address) & 0xFFFF)
 
 #define high_16(address) (u16)(((address) >> 16) & 0xFFFF)
