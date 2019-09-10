0cfbb561d0b04576fbbfe890f9db429d8165d0ce
Mark <clashclanacc2602@gmail.com>
2019-09-10 20:13:15 +0300

    add irq handler( or try it)

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -7,47 +7,92 @@
 #include "app.h"
 #include "log.h"
 #include "types.h"
+// a pointer to the global descriptor table
+// passed by reference to the LGDT instruction
 
-struct gdt_t {
-  int32 address;
-  int16 size;
-} __attribute__((packed)) gdt;
+struct gdt_description_structure_t {
+  uint16_t size; // in bytes
+  uint32_t offset;
+} __attribute__((packed)) gdt_description_structure;
+
+// a pointer to the interrupt descriptor table
+// passed by reference to the LIDT instruction
+
+struct idt_description_structure_t {
+  uint16_t size; // in bytes
+  uint32_t offset;
+} __attribute__((packed)) idt_description_structure;
 
 // See http://wiki.osdev.org/Global_Descriptor_Table#Structure
+
 struct segment_descriptor_t {
-  int16 base_0_15; // bits 0-15 of base
-  int16 limit_0_15;
-  int8 base_24_31;
-  int8 flags_and_limit_16_19;
-  int8 access_byte;
-  int8 base_16_23;
+  uint16_t limit_0_15; // bits o-15 of limit
+  uint16_t base_0_15;
+  uint8_t base_16_23;
+  uint8_t access_byte;
+  uint8_t flags_and_limit_16_19;
+  uint8_t base_24_31;
+} __attribute__((packed));
+
+// See http://wiki.osdev.org/Interrupt_Descriptor_Table#Structure_IA-32
+struct interrupt_descriptor_t {
+  uint16_t offset_0_15;
+  uint16_t selector;
+  uint8_t zero;
+  uint8_t type_attr;
+  uint16_t offset_16_31;
 } __attribute__((packed));
 
 enum segment_selector_t {
   NULL_DESCRIPTOR, // Not but has to be here
-  KERNAL_CODE_SEGMENT, // Offset 0x8
-  KERNAL_DATA_SEGMENT // Offset 0x10
+  KERNAL_CODE_SEGMENT_INDEX, // Offset 0x8
+  KERNAL_DATA_SEGMENT_INDEX // Offset 0x10
 };
-struct segment_descriptor_t segment_descriptors[3];
-
+struct segment_descriptor_t gdt[3];
+const uint16_t NULL_SEGMENT_SELECTOR = 0x0;
+const uint16_t KERNAL_CODE_SEGMENT_SELECTOR = sizeof(struct segment_descriptor_t) * 1;
+const uint16_t KERNAL_DATA_SEGMENT_SELECTOR = sizeof(struct segment_descriptor_t) * 2;
+struct interrupt_descriptor_t idt[256];
 void initialize_gdt() {
-  gdt.address = (int16) segment_descriptors;
-  gdt.size = sizeof(segment_descriptors);
+  gdt_description_structure.size = sizeof(gdt) - 1;
+  gdt_description_structure.offset = (uint32_t) gdt;
+  gdt[KERNAL_CODE_SEGMENT_INDEX].limit_0_15 = 0xFFFF;
+  gdt[KERNAL_CODE_SEGMENT_INDEX].base_0_15 = 0x0000;
+  gdt[KERNAL_CODE_SEGMENT_INDEX].base_16_23 = 0x00;
+  gdt[KERNAL_CODE_SEGMENT_INDEX].access_byte = 0b10011010;
+  gdt[KERNAL_CODE_SEGMENT_INDEX].flags_and_limit_16_19 = 0xCF;
+  gdt[KERNAL_CODE_SEGMENT_INDEX].base_24_31 = 0x00;
+  gdt[KERNAL_DATA_SEGMENT_INDEX].limit_0_15 = 0xFFFF;
+  gdt[KERNAL_DATA_SEGMENT_INDEX].base_0_15 = 0x0000;
+  gdt[KERNAL_DATA_SEGMENT_INDEX].base_16_23 = 0x00;
+  gdt[KERNAL_DATA_SEGMENT_INDEX].access_byte = 0b10010010;
+  gdt[KERNAL_DATA_SEGMENT_INDEX].flags_and_limit_16_19 = 0xCF;
+  gdt[KERNAL_DATA_SEGMENT_INDEX].base_24_31 = 0x00;
+  lgdt(&gdt_description_structure);
 
-  segment_descriptors[KERNAL_CODE_SEGMENT].base_0_15 = 0x0;
-  segment_descriptors[KERNAL_CODE_SEGMENT].base_24_31 = 0x0;
-  segment_descriptors[KERNAL_CODE_SEGMENT].limit_0_15 = 0xFFFF;
-  segment_descriptors[KERNAL_CODE_SEGMENT].flags_and_limit_16_19 = 0b1000 << 4;
-  segment_descriptors[KERNAL_CODE_SEGMENT].flags_and_limit_16_19 &= 0xF;
-  segment_descriptors[KERNAL_CODE_SEGMENT].access_byte &= 0b10010110;
+  // Grub has already loaded the segment register
+  // with the correct values (0x8 for cs, 0x10 for the others)
+}
 
-  segment_descriptors[KERNAL_DATA_SEGMENT].base_0_15 = 0x0;
-  segment_descriptors[KERNAL_DATA_SEGMENT].base_24_31 = 0x0;
-  segment_descriptors[KERNAL_DATA_SEGMENT].limit_0_15 = 0xFFFF;
-  segment_descriptors[KERNAL_DATA_SEGMENT].flags_and_limit_16_19 = 0b1000 << 4;
-  segment_descriptors[KERNAL_DATA_SEGMENT].flags_and_limit_16_19 &= 0xF;
-  segment_descriptors[KERNAL_DATA_SEGMENT].access_byte &= 0b10010010;
-  lgdt(&gdt);
+void initialize_idt() {
+  idt_description_structure.size = sizeof(idt) - 1;
+  idt_description_structure.offset = (uint32_t) idt;
+  uint32_t interrupt_handler_address = (uint32_t) &interrupt_handler;
+  uint16_t offset_0_15 = interrupt_handler_address & 0x0000FFFF;
+  uint16_t offset_16_31 = interrupt_handler_address >> 16;
+  uint16_t selector = KERNAL_CODE_SEGMENT_SELECTOR;
+  uint8_t zero = 0x00;
+  uint8_t type_attr = 0x8E;
+  for (int i = 0; i < 256; i++) {
+    idt[i].offset_0_15 = offset_0_15;
+    idt[i].selector = selector;
+    idt[i].zero = zero;
+    idt[i].type_attr = type_attr;
+    idt[i].offset_16_31 = offset_16_31;
+  }
+
+  load_idt(&idt_description_structure);
+}
 
   // Grub has already loaded the segment registers
   // with the correct values (0x8 for cs, 0x10 for the others)
@@ -55,10 +100,13 @@ void initialize_gdt() {
 
 void kernel_main(void) 
 {
+  initialize_gdt();
+  initialize_idt();
 	init();
 	log("Hello World!\n");
 	log("Loaded global descriptor table.\n");
 	log("Loading app\n");
+  interrupt(49);
 	printf("Kernel module OK. Loading app...");
 	halt();
 }
