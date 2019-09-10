485a9cf18e51f4fe7cf46e173589d7b8fda6e68d
Mark <53225591+nergzd723@users.noreply.github.com>
2019-09-10 18:29:51 +0300

    Update typo.h

--- include/irq/typo.h
+++ include/irq/typo.h
@@ -8,5 +8,10 @@ typedef unsigned char  u8;
 typedef          char  s8;
 #define low_16(address) (u16)((address) & 0xFFFF)
 #define high_16(address) (u16)(((address) >> 16) & 0xFFFF)
-
+typedef unsigned char int8;
+typedef int8 byte;
+typedef unsigned short int16;
+typedef int16 byte2;
+typedef unsigned long int32;
+typedef int16 byte4;
 #endif
