23531a2998e5164874c4143483df294764ee7e68
Mark <53225591+nergzd723@users.noreply.github.com>
2019-09-10 18:09:54 +0300

    Create log.h

---
+++ include/log.h
@@ -0,0 +1,41 @@
+#ifndef LOGH_H
+#define LOGH_H
+#include "rw.h"
+#include "serialif.h"
+#include "oopsh.h"
+#include "serial.h"
+
+void logd(long data)
+{
+  char* c = inttostr(data);
+  log(c);
+  log(" ");
+}
+void cls()
+{
+  clsdrv();
+}
+void init()
+{
+  t_init();
+  serial_init(SERIAL_COM1_BASE);
+  log("GDT, terminal and serial init!");
+}
+void log(char* data)
+{
+  serial_write(SERIAL_COM1_BASE, data);
+  serial_write(SERIAL_COM1_BASE, " ");
+}
+void printf(char* data)
+{
+  write(data);
+  log("Wrote to screen");
+}
+void warn(char* data)
+{
+  log(data);
+  log("Warning!");
+  write("\n\n\nWarning!\n");
+  write(data);
+}
+#endif
