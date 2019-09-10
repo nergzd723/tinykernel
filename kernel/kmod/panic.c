4ef606c67db8fc0b4ce12abf780e33dfbda6f2d7
Mark <53225591+nergzd723@users.noreply.github.com>
2019-09-10 18:23:48 +0300

    Update panic.c

--- kernel/kmod/panic.c
+++ kernel/kmod/panic.c
@@ -4,13 +4,13 @@
 #include "serialif.h"
 #include "dsp.h"
 void oops(char* errmsg){
-    log(SERIAL_COM1_BASE, "Got exception, \n Panicked successfully!");
-    log(SERIAL_COM1_BASE, errmsg);
+    log("Got exception, \n Panicked successfully!");
+    log(errmsg);
     clsdrv();
     write("\n\n\n\n\n\n\n\n\n\n");
     write("An error occured!\nThe error code is");
     write("\n\n");
     write(errmsg);
-    log(SERIAL_COM1_BASE, "Halting the CPU...");
+    log("Halting the CPU...");
     halt();
 }
